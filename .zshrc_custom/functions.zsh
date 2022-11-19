fff977cc0e9462f44d88f4a34e3cb41571a39738
Pablo Valero <pabloevd950@icloud.com>
2022-11-19 11:10:14 -0600

    Added quick-look function

--- .zshrc_custom/functions.zsh
+++ .zshrc_custom/functions.zsh
@@ -29,3 +29,7 @@ function myip() {
 	ifconfig en1 | grep 'inet ' | sed -e 's/:/ /' | awk '{print "en1 (IPv4): " $2 " " $3 " " $4 " " $5 " " $6}'
 	ifconfig en1 | grep 'inet6 ' | sed -e 's/ / /' | awk '{print "en1 (IPv6): " $2 " " $3 " " $4 " " $5 " " $6}'
 }
+
+function quick-look() {
+  (( $# > 0 )) && qlmanage -p $* &>/dev/null &
+}
