1e18600bf01b34f0f1ca60a5163a230a43f70375
Pablo Valero <pevd950@github.com>
2022-11-21 09:52:11 -0600

    Idk

--- .zshrc_custom/alias.zsh
+++ .zshrc_custom/alias.zsh
@@ -16,7 +16,7 @@ alias gm="git merge"
 alias gcb="git checkout -b"
 alias gclone="git clone"
 alias gt="gittower ."
-alias gcr
+alias gcr="git rebase --continue"
 
 # GitHub
 # gh alias set --shell startcs 'gh cs code -c $(gh cs list --json "name" | jq -r ".[].name" | grep -i "$1" -m 1)'
@@ -30,17 +30,25 @@ alias yadd="yadm add"
 
 # Editor
 alias zshrc="code ~/.zshrc"
+alias nvzshrc='nvim ~/.zshrc'
+alias nvaliases='nvim "$ZSH_CUSTOM"/alias.zsh'
 alias myaliases='code "$ZSH_CUSTOM"/alias.zsh'
 alias myfunctions='code "$ZSH_CUSTOM"/functions.zsh'
 alias myplugins='code "$ZSH_CUSTOM"/plugins.zsh'
 alias myzshrc='code "$ZSH_CUSTOM"'
 
-alias ccat='bat -pp'
-alias lls='exa --icons -s=Name'
+# Command Overrides
+alias cat='bat -pp'
+alias ls='exa --icons -s=Name'
+alias nv='nvim'
+alias ql='quick-look'
+alias pman='preman'
 
-# Apps
+## Apps
+
 # MacUpdater
 alias macupdater='/Applications/MacUpdater.app/Contents/Resources/macupdater_client'
-# VsCode
+
+# VSCode
 alias vsc='code .'
 alias coden='code --new-window'
