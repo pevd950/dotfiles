da8456a68c41c6b0069bed17e82c49381e3625cd
Pablo Valero <12861801+pevd950@users.noreply.github.com>
2023-06-07 16:11:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ install_oh_my_zsh() {
 
 # Define a function to install Starship
 install_starship() {
-  curl -sS https://starship.rs/install.sh | sh --yes
+  curl -sS https://starship.rs/install.sh | sh
 }
 
 # Define a function to install rbenv
