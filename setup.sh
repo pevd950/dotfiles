59cc7ff137e24b632dff312534055bda66302d71
Pablo Valero <pabloevd950@icloud.com>
2026-05-20 12:37:16 -0500

    Use return in setup shell phase

--- setup.sh
+++ setup.sh
@@ -234,7 +234,7 @@ setup_shell() {
 
   # Install Oh My Zsh plugins
   local ZSH_HIGHLIGHT_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-  install_zsh_syntax_highlighting "$ZSH_HIGHLIGHT_DIR" || exit 1
+  install_zsh_syntax_highlighting "$ZSH_HIGHLIGHT_DIR" || return 1
 }
 
 main() {
